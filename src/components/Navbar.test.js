80b1149f9f2ebf4736d8b2be12102bac044e3a87
Aliadabi <Aliadabi3780@gmail.com>
2020-06-11 16:15:50 -0700

    All Done

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 import Navbar from './Navbar';
-
+import { useDarkMode } from "../hooks/useDarkMode"
 test('renders Navbar without crashing', () => {
+  
   rtl.render(<Navbar />);
 });
